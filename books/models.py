c91faf33316282278d1c5e158112f86984dac762
FranciscMoldovan <franciscmoldovan93@gmail.com>
2019-03-25 22:55:57 +0200

    Just created Book model

--- books/models.py
+++ books/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    release_date = models.DateField
+    author = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
+    genre = models.CharField(max_length=150)
